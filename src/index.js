45415cd3dbf756c739df95525ceda6873dbe5bd3
Natalia <safary-1994@mail.ru>
2020-10-16 22:13:00 +0300

    feat: wrong decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+function peek(stack) {
+  return stack[stack.length - 1]; 
+}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  
+  let stack = [];
+  stack.push();
+  stack.pop();
+
+  for (let i = 0; i < str.length; i++) {
+    let letter = str.charAt(i);
+    if (letter === '(' 
+    || letter === '['
+    || letter === '{' ) {
+      stack.push(letter);
+    } else if (letter === ')' || letter === ']'
+    || letter === '}') {
+      if (peek(stack) === '(' || peek(stack) === '['
+      || peek(stack) === '{' || peek(stack) === '(]') {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
